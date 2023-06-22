f27daadea2795b3cad2bd5a8498b6bff3bd00697
Razeet07 <razit44mhj@gmail.com>
2023-06-22 22:45:54 +0545

    assignmnet on array methods

--- Assginment/JavaScript/Array/array.js
+++ Assginment/JavaScript/Array/array.js
@@ -1,3 +1,65 @@
-const myArray = [24, "razeet",1991]
-myArray.pop(25)
-console.log(myArray)
+
+//Q1 find if hari is in allUsers array or not/
+//expected output is : 
+//true
+
+
+// method : 01
+var strings = ['hari','shyam','hari','thakur'];
+var searchString = "hari";
+var found = false;
+//using loop method to find specific string --> array
+for (var i = 0; i < strings.length; i++) {
+  if (strings[i] === searchString) {
+    found = true;
+    break;
+  }
+}
+
+if (found) {
+  console.log("hari' found in the array.", true);
+} else {
+  console.log("'hari' not found in the array.",false);
+}
+
+
+//method-02
+
+const string = 'hari'
+const allUsers = ['hari','shyam','thakur']
+var found = false;
+//using array "find method" to seach any string from array.
+const findHari = allUsers.find(element => element === "hari");
+
+if (findHari !== string){
+  found = false;
+  console.log(false, 'string not found')
+}else{
+  console.log(findHari,true)
+}
+
+// -----------------------------------------------------------------------------------------------------------------------------------
+
+//Q2 find number of duplicates
+//expected output is : 2
+//true
+const user= 'hari'
+const userArr = ['hari','shyam','hari','thakur']
+
+
+const newArr = userArr
+  .map((userArr) => {
+    return {
+      count: 1,
+      name: userArr
+    };
+  })
+  
+  .reduce((result, b) => {
+    result[b.name] = (result[b.name] || 0) + b.count;
+
+    return result;
+  }, {});
+const duplicates = Object.keys(uniq).filter((a) => uniq[a] > 1);
+
+console.log(duplicates); 
